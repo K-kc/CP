c838d4473bf4991141bbb2fbb7f17bba36c96640
Kunal Chaudhari <103252312+K-kc@users.noreply.github.com>
2022-04-29 16:51:12 +0530

    Add files via upload

---
+++ P4_Variables.c
@@ -0,0 +1,19 @@
+//Program 4
+
+#include <stdio.h>
+void add()
+{
+int a=4;
+int b=3;
+int c=a+b;
+printf("sum of a and b is %d\n",c);
+}
+void main()
+{
+    add();
+}
+
+
+//Output
+
+//sum of a and b is 5
