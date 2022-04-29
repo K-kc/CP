eea4ba9b986b86b5f2e269733908351de0f6b68c
Kunal Chaudhari <103252312+K-kc@users.noreply.github.com>
2022-04-29 16:41:23 +0530

    Add files via upload

---
+++ P3_Functions.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int a=7;
+int b=11;
+void add()
+{
+int sum;
+sum=a+b;
+printf("sum of a and b is %d\n",sum);
+}
+int main()
+{
+add();
+return 0;
+}
+
+//Output
+//sum of a and b is 18
