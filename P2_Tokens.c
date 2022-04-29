831586a52071e87ae0b34be656d8b316184216fd
Kunal Chaudhari <103252312+K-kc@users.noreply.github.com>
2022-04-29 16:26:15 +0530

    Add files via upload

---
+++ P2_Tokens.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+ int
+main () 
+ 
+{
+  
+ 
+int a = 3;
+  
+ 
+double const b = 8;
+  
+ 
+float c = 1.5;
+  
+ 
+char d = 'A';
+  
+ 
+printf ("the value of a is %d\n", a);
+  
+ 
+printf ("the value of b is %lf\n", b);
+  
+ 
+printf ("the value of c is %.2f\n", c);
+  
+ 
+printf ("the value of d is %c\n", d);
+  
+ 
+return 0;
+
+ 
+}
+
+//Output:
+//the value of a is 3
+//the value of b is 8.000000
+//the value of c is 1.50
+//the value of d is A
