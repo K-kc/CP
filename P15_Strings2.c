3bed0ae1e1d625fc98562749b2a2e59bb7a3cda8
Kunal Chaudhari <103252312+K-kc@users.noreply.github.com>
2022-05-27 15:27:19 +0530

    Create P15_Strings2.c

---
+++ P15_Strings2.c
@@ -0,0 +1,12 @@
+INPUT:
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char strname[30]="c-programmimg";
+    printf("the string is %s\n",strname);
+    return 0;
+}
+
+// OUTPUT:
+// the string is c-programmimg
