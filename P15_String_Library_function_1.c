6b98783b4f0fe4b29867e603045db77f253825b4
Kunal Chaudhari <103252312+K-kc@users.noreply.github.com>
2022-06-15 23:35:46 +0530

    Create P15_String_Library_function_1.c

---
+++ P15_String_Library_function_1.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char arr[]="Kunal";
+    int len1 = strlen (arr);
+    printf ("string =%s\nlength=%d\n",arr,len1);
+    return 0;
+}
+
+
+
+/*Output-
+string =Kunal
+length=6*/
